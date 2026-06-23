4728d8ecb446545fb875e57b11e8f2d10d520957
Victor <ventingabrol@gmail.com>
2026-06-23 16:28:10 -0300

    opa

---
+++ Scripts/player.gd
@@ -0,0 +1,32 @@
+extends RigidBody2D
+
+@export var impulso = 1.0
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	gravity_scale = 0
+	linear_damp = 1
+	angular_damp = 1
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("impulse"):
+		darImpulso()
+		anim.play("impulse")
+	else:
+		anim.play("idle")
+
+	rotate360()
+
+func darImpulso():
+	var direction = Vector2.UP.rotated(rotation)
+	apply_central_impulse(direction * impulso)
+
+func rotate360():
+	if Input.is_action_pressed("left"):
+		apply_torque(-200)
+	elif Input.is_action_pressed("right"):
+		apply_torque(200)
